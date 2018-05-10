95ff0082c158a6dae88fea8d1a432b405fea6d94
tgmuender <tgmuender@gmail.com>
2018-05-10 11:07:29 +0200

    Move thumbs & metadata to separate subdirectory

--- prepareImages.py
+++ prepareImages.py
@@ -9,6 +9,12 @@ import json
 import datetime
 
 
+class GalleryImage:
+
+    def __init__(self, name):
+        self.name = name
+
+
 def get_exif_meta_data(file_name):
     exif_data = {}
     exif_dict = piexif.load(file_name)
@@ -20,13 +26,23 @@ def get_exif_meta_data(file_name):
 
 
 def write_image_meta_data(file_name, img_meta_data):
-    with open(file_name + ".json", 'w') as outfile:
+    image_name = os.path.basename(file_name)
+    with open(image_dir + '/metadata/' + image_name + ".json", 'w') as outfile:
         json.dump(img_meta_data, outfile)
 
 
 def create_thumbnail(file_name):
     image = cv2.imread(file_name)
-    thumbnail = cv2.resize(image, (120, 120))
+
+    height, width = image.shape[:2]
+    thumbnail_max_width = 250
+    thumbnail_max_height = 250
+
+    if thumbnail_max_height < height or thumbnail_max_width < width:
+        scaling_factor = thumbnail_max_height / float(height)
+        if thumbnail_max_width/float(width) < scaling_factor:
+            scaling_factor = thumbnail_max_width / float(width)
+    thumbnail = cv2.resize(image, None, fx=scaling_factor, fy=scaling_factor, interpolation=cv2.INTER_AREA)
     cv2.imwrite(image_dir + "/thumbs/thumb_%s" % basename, thumbnail)
 
 
@@ -39,12 +55,18 @@ def get_creation_timestamp(exif_data):
 
 image_dir = sys.argv[1]
 print("Scanning directory " + image_dir)
-path = image_dir + '/*.jpg'
+path = image_dir + '/*'
 files = glob.glob(path)
 images = []
 for file in files:
+    if not file.endswith((".JPG", ".jpg")):
+        continue
+
     if not os.path.exists(image_dir + '/thumbs'):
         os.makedirs(image_dir + '/thumbs')
+
+    if not os.path.exists(image_dir + '/metadata'):
+        os.makedirs(image_dir + '/metadata')
 
     basename = os.path.basename(file)
     print("Found image: " + basename)
@@ -58,7 +80,9 @@ for file in files:
 images.sort(key=lambda img_tpl: get_creation_timestamp(img_tpl[1]).timestamp())
 sorted_file_names = []
 for img in images:
-    sorted_file_names.append(os.path.basename(img[0]))
+    sorted_file_names.append(GalleryImage(os.path.basename(img[0])))
 
-with open(image_dir + "/sort-order.json", 'w') as outfile:
-    json.dump(sorted_file_names, outfile)
+with open(image_dir + "/images.json", 'w') as outfile:
+    json.dump(sorted_file_names, outfile, default=lambda x: x.__dict__)
+
+
